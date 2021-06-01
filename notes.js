1a37665291d351b036aedc3bfb2b19f70fc93362
DouglasPenn23 <63472155+DouglasPenn23@users.noreply.github.com>
2021-06-01 10:33:48 -0400

    Data & Algo Notes

---
+++ notes.js
@@ -0,0 +1,34 @@
+// Creates a var for the count
+var count = 0;
+
+// Creates function for card counting that takes in a card
+function cc(card) {
+  // Only change code below this line
+  // Function will Recieve Card Parameter - Done
+  // Writes a switch function 
+switch (card){
+    // For cases 2-6, increment the count and break. 
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+  case 6:
+  count ++;
+  break;
+  // For cases 10 & Face cards, decrement count & break. 
+  case 10:
+  case 'J':
+  case 'Q':
+  case 'K':
+  case 'A':
+  count --;
+  break;
+}
+// If count is greater than 0, bet. 
+  if(count > 0){
+    return count + " Bet";
+  }
+  else{
+      // If count is less than 0. 
+    return count + " Hold";
+  }
